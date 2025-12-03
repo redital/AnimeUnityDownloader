0ef0bfed2ba0cffa53d1de1d3a1910c6eb68e271
Raffaello Ippolito <86683344+redital@users.noreply.github.com>
2025-12-03 21:51:14 +0100

    config fix

--- config.py
+++ config.py
@@ -14,9 +14,9 @@ static_folder = os.environ.get("STATIC_FOLDER_PATH",'web_app/static')
 nas_mac_address = os.environ.get("NAS_MAC_ADDRESS", "placeholder")
 nas_host_name = os.environ.get("NAS_HOST_NAME", "placeholder")
 
-omv_user = os.environ.get("", "placeholder")
-omv_password = os.environ.get("", "placeholder")
-mount_path = os.environ.get("", "placeholder")
+omv_user = os.environ.get("OMV_USER", "placeholder")
+omv_password = os.environ.get("OMV_PASSWORD", "placeholder")
+mount_path = os.environ.get("MOUNT_PATH", "placeholder")
 
 ssh_host = os.environ.get("SSH_HOST", "placeholder")
 ssh_user = os.environ.get("SSH_USER", "placeholder")
